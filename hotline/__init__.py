436b92c965028eddc98a61920b90d6f6fd508a75
Dan Bradham <danielbradham@gmail.com>
2015-11-12 15:52:25 -0500

    0.5.1

--- hotline/__init__.py
+++ hotline/__init__.py
@@ -2,7 +2,7 @@ __title__ = "hotline"
 __author__ = "Dan Bradham"
 __email__ = "danielbradham@gmail.com"
 __url__ = "http://github.com/danbradham/hotline"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __license__ = "MIT"
 __description__ = "A pop-up input field with customizable settings."
 
