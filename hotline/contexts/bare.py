8f788e183e5c530e609d28d2ae588cf1d49d93e7
Dan Bradham <danielbradham@gmail.com>
2017-03-31 01:04:24 -0400

    Update python mode

--- hotline/contexts/bare.py
+++ hotline/contexts/bare.py
@@ -11,10 +11,17 @@ class Python(Mode):
     name = 'Python'
     short_name = 'PY'
     commands = []
+    prompt = 'python command'
 
     def execute(self, command):
-        code = compile(command, '<string>', 'exec')
-        exec code in sys.modules['__main__'].__dict__
+        main = sys.modules['__main__'].__dict__
+        try:
+            code = compile(command, '<string>', 'eval')
+            return eval(code, main, main)
+        except SyntaxError:
+            code = compile(command, '<string>', 'exec')
+            exec code in main
+
 
 
 class BareContext(Context):
